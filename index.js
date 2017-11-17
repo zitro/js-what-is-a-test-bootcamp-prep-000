1061c4dd03ab63b92bc6cf7471918fa048a592bc
Bryan Ortiz <bryan@bryanortiz.com>
2017-11-17 04:41:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,26 @@ var name = "Joe"
 var height = 74
 var message = `${name} is ${height} inches tall`
 
+function what-is-a-test(name){
+if (name === 'Susan'){
+    return 'Susan'
+}else{
+  return 'We expect the name to equal Susan.'
+}
+
+
+function Height(height){
+  if( height < 40){
+    return `Height is  ${height}`
+  }else {
+    return 'We expect the height to be less than 40.'
+  }
+}
+
+function Message(name, height){
+return `${name}, is ${} inches tall.`  
+}
+
+
+
 module.exports = {name, height, message}
